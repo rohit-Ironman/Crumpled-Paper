edcc161c6a4285af9fb13d3ffedebc91097dc205
rohit-Ironman <72317546+rohit-Ironman@users.noreply.github.com>
2020-10-29 21:37:38 -0700

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,33 +1,29 @@
-class Paper{
-    contructor(x,y){
-        var options ={
-            isStatic:false, 
-            'restitution': 0.3,
-            'friction': 0.5,
-            'density': 1.2
-        }
+class Paper {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 20, 20, options);
+      this.width = 20;
+      this.height = 20;
 
-        this.body = Bodies.rectangle(x,y,20,20,options);
-        this.width = 20;
-        this.height = 20;
 
-        World.add(world, this.body);
+
+      World.add(world, this.body);
     }
-
     display(){
-        var pos = this.body.position;
-    push();
-    translate(pos.x, pos.y);
-    strokeWeight(5);
-    stroke("white"); 
-    rectMode(CENTER);
-    fill("pink");
-    rect(0,0,this.width, this,height); 
-    pop();
-
-
-
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke("white");
+      rectMode(CENTER);
+      fill("red");
+      rect(0, 0, this.width, this.height);
+      pop();
     }
-
-
-}
+  };
--- sketch.js
+++ sketch.js
@@ -4,14 +4,11 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var paper; 
-var trashBase; 
-var trashSide1, trashSide2;
+var paper, ground; 
+var block1; 
+var block2, block3;
 
-function preload()
-{
-	
-}
+
 
 function setup() {
 	createCanvas(800, 700);
@@ -20,8 +17,17 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	paper = new Paper(200, 650); 
+	paper = new Paper(200, 556); 
+	ground = new Ground(400,600,800,20);
 
+	block1 = createSprite(600,596,200,10);
+	block1.shapeColor = color("red");
+
+	block2 = createSprite(500,560,10,100);
+	block2.shapeColor = color("red"); 
+
+	block3 = createSprite(700,560,10,100);
+	block3.shapeColor = color("red");
 
 	Engine.run(engine);
   
@@ -32,11 +38,19 @@ function draw() {
 
   background(0);
 	Engine.update(engine);
-  paper.display();
 
-  drawSprites();
+  ground.display();
+  paper.display();
+ 
+drawSprites();
+
+
  
 }
 
-
-
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+	  Matter.Body.applyForce(paper.body,paper.body.position,{x:7,y:-28});
+	}
+  }
+  
